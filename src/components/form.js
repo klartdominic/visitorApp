1933ad019a7176f3dc557de9b99fe64de59a94a0
Klart Dominic <klartdominic@gmail.com>
2020-02-12 09:07:52 +0800

    async storage

--- src/components/form.js
+++ src/components/form.js
@@ -56,26 +56,46 @@ class Form extends Component{
       isError: true,
       curDate: '',
       keyDate: '',
-    }
+    };
   }
 
   componentDidMount(){
     //for testing only
-    this.setState({
-      inputName:    {text: 'testInputName', valid:true},
-      inputID:      {text: 'testinputID', valid:true},
-      inputPerson:  {text: 'testinputPerson', valid:true},
-      inputPurpose: {text: 'testinputPurpose', valid:true},
-      inputIDNo: {text: 'testinputIDNo', valid:true},
-      inputHost: {text: 'tesinputHost', valid:true},
-    })
+    // this.setState({
+    //   inputName: {text: 'testInputName1', valid: true},
+    //   inputID: {text: 'testinputID', valid: true},
+    //   inputPerson: {text: 'testinputPerson', valid: true},
+    //   inputPurpose: {text: 'testinputPurpose', valid: true},
+    //   inputIDNo: {text: 'testinputIDNo', valid: true},
+    //   inputHost: {text: 'tesinputHost', valid: true},
+    // });
 
     this.state.curDate = new Date().toLocaleString();
   }
 
-  _scrollToInput (reactNode: any) {
+  _scrollToInput(reactNode: any) {
     // Add a 'scroll' ref to your ScrollView
     this.scroll.props.scrollToFocusedInput(reactNode)
+  }
+
+  storeData = async (key, DATA) => {
+    try {
+      await AsyncStorage.setItem(key, DATA);
+      // alert('Data successfully saved!');
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  retrieveItem = async (key) => {
+    try {
+      const retrievedItem =  await AsyncStorage.getItem(key);
+      const item = JSON.parse(retrievedItem);
+      return item;
+    } catch (error) {
+      console.log(error.message);
+    }
+    return;
   }
 
   logData = (states) => {
@@ -88,35 +108,23 @@ class Form extends Component{
       purpose: states.inputPurpose.text,
       idNo: states.inputIDNo.text,
       host: states.inputHost.text,
-    }
+    };
 
-    if ( this.isValidateAll() ){
-      console.log(JSON.stringify(setObject));
+    if (this.isValidateAll()) {
+      console.log(this.storeData('@visitor_key', JSON.stringify(setObject)));
+      // console.log(JSON.stringify(setObject));
+      // console.log(this.retrieveItem('@visitor_key'));
     }else{
       this.checkValidation('Name', 'Full Name') ;
       this.checkValidation('Person', 'Person to Visit');
       this.checkValidation('Purpose', 'Purpose');
       this.checkValidation('IDNo', 'ID No.');
-      Alert.alert(
-        'Invalid',
-        "Please check highligted fields"
-      );
+      Alert.alert('Invalid', 'Please check highlighted fields');
     }
-  }
-
-  async retrieveItem() {
-    try {
-      const retrievedItem =  await AsyncStorage.getItem('visitor');
-      const item = JSON.parse(retrievedItem);
-      return item;
-    } catch (error) {
-      console.log(error.message);
-    }
-    return
-  }
+  };
 
   validateInput(text,fieldName){
-    let _field = `input${fieldName}`
+    let _field = `input${fieldName}`;
     let setObject = {
       text: text,
       valid: text.length !== 0,
@@ -130,25 +138,26 @@ class Form extends Component{
     let _errText = `* ${text} is required`;
     let _isValid = this.state[_object].valid;
 
-    _isValid 
-      ? this.setState({ [_errObj]: null }) 
-      : this.setState({ [_errObj]: _errText });
-    
-  }
+    _isValid
+      ? this.setState({[_errObj]: null})
+      : this.setState({[_errObj]: _errText});
+  };
 
   isValidateAll = () => {
-    if (this.state.inputName.valid == true && 
-        this.state.inputPerson.valid == true &&
-        this.state.inputPurpose.valid == true &&
-        this.state.inputIDNo.valid == true){
+    if (
+      this.state.inputName.valid === true &&
+      this.state.inputPerson.valid === true &&
+      this.state.inputPurpose.valid === true &&
+      this.state.inputIDNo.valid === true
+    ){
       return true;
-    }else{
+    } else {
       return false;
-    };
-  }
+    }
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.formContainer}>
         <TextInput 
           style={styles.textInput}
@@ -160,34 +169,34 @@ class Form extends Component{
         <Text style={styles.errText}>{this.state.errName}</Text>
         <TextInput 
           style={styles.textInput}
-          placeholder="Identification Presented" 
+          placeholder="Identification Presented"
           onChangeText={(inputID) => this.validateInput(inputID, 'ID')}
           ref={(input) => this.inputID = input}
           onSubmitEditing={() => this.inputPerson.focus()}
         />
         <Text style={styles.errText}>{this.state.errID}</Text>
-        <TextInput 
+        <TextInput
           style={styles.textInput}
-          placeholder="Person to Visit" 
+          placeholder="Person to Visit"
           onChangeText={(Person) => this.validateInput(Person, 'Person')}
           ref={(input) => this.inputPerson = input}
           onSubmitEditing={() => this.inputPurpose.focus()}
           onBlur={() => this.checkValidation('Person', 'Person to Visit')}
         />
         <Text style={styles.errText}>{this.state.errPerson}</Text>
-        <TextInput 
+        <TextInput
           style={styles.detailInput}
           multiline={true}
           numberOfLines={5}
-          maxLength={1000} 
-          placeholder="Purpose" 
+          maxLength={1000}
+          placeholder="Purpose"
           onChangeText={(Purpose) => this.validateInput(Purpose, 'Purpose')}
           ref={(input) => this.inputPurpose = input}
           onSubmitEditing={() => this.inputIDNo.focus()}
           onBlur={() => this.checkValidation('Purpose', 'Purpose')}
         />
         <Text style={styles.errText}>{this.state.errPurpose}</Text>
-        <TextInput 
+        <TextInput
           style={styles.textInput}
           placeholder="ID No." 
           onChangeText={(IDNo) => this.validateInput(IDNo, 'IDNo')}
@@ -196,9 +205,9 @@ class Form extends Component{
           onBlur={() => this.checkValidation('IDNo', 'ID No.')}
         />
         <Text style={styles.errText}>{this.state.errIDNo}</Text>
-        <TextInput 
+        <TextInput
           style={styles.textInput}
-          placeholder="Host Name" 
+          placeholder="Host Name"
           onChangeText={(Host) => this.validateInput(Host, 'Host Name')}
           ref={(input) => this.inputHost = input}
         />
@@ -206,12 +215,11 @@ class Form extends Component{
         
         <TouchableOpacity 
           style={styles.touchable}
-          onPress={() => this.logData(this.state)}
-        >
+          onPress={() => this.logData(this.state)}>
           <Text>Login</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
